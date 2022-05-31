42038d52e44670f6c55973e3e591b3bc23144c91
Jared Hanson <jaredhanson@gmail.com>
2022-05-31 12:08:35 -0700

    Clean up.

--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,49 @@
 window.addEventListener('load', function() {
 
   if (window.PasswordCredential) {
-    navigator.credentials
-      .get({
-        password: true,
-        mediation: 'required'
-      })
-      .then(function(credential) {
-        if (!credential) { return; }
-      
-        if (credential.type === 'password') {
-          document.querySelector('input[name=username]').value = credential.id;
-          document.querySelector('input[name=password]').value = credential.password;
-          document.querySelector('form').submit();
-        }
-      });
+    navigator.credentials.get({
+      password: true,
+      mediation: 'required'
+    })
+    .then(function(credential) {
+      if (!credential) { return; }
     
-    document.querySelector('form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      
-      fetch('/login/password', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json'
-        },
-        body: new FormData(event.target),
-      }).then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        if (json.ok) {
-          var credential = new PasswordCredential(event.target);
-          navigator.credentials.store(credential)
-            .catch(function(error) {
-              console.log(error)
-            });
-        }
-        
-        window.location.href = json.location;
-      }).catch(function(error) {
-        console.log(error);
-      });
+      if (credential.type === 'password') {
+        document.querySelector('input[name=username]').value = credential.id;
+        document.querySelector('input[name=password]').value = credential.password;
+        document.querySelector('form').submit();
+      }
     });
   }
+    
+  document.querySelector('form').addEventListener('submit', function(event) {
+    if (!window.PasswordCredential) { return; }
+    
+    event.preventDefault();
+    
+    fetch('/login/password', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      },
+      body: new FormData(event.target),
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      if (json.ok) {
+        var credential = new PasswordCredential(event.target);
+        navigator.credentials.store(credential)
+        .catch(function(error) {
+          console.log(error)
+        });
+      }
+      
+      window.location.href = json.location;
+    }).catch(function(error) {
+      console.log(error);
+    });
+  });
   
 });
